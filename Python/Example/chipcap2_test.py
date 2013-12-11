80ec6cfe41ab097165f69d88d367560924c75528
Richard Wardlow <richard@circuitsforfun.com>
2013-12-11 00:18:54 -0500

    Initial Commit

---
+++ Python/Example/chipcap2_test.py
@@ -0,0 +1,50 @@
+
+import sys
+
+#sys.path.append('H:/GitHub/ChipCap2/Python/Library') # set your path to the ChipCap2 lib if you didn't copy it to your site-packages folder
+import pymcu # import pyMCU library
+import CFF_ChipCap2  # import ChipCap2 library
+
+mb = pymcu.mcuModule() # init pyMCU class
+
+ChipCap2_i2cAddr = 0x28
+ChipCap2_PowerPin = 1
+ChipCap2_ReadyPin = 2
+ChipCap2_AlarmLowPin = 3
+ChipCap2_AlarmHighPin = 5
+
+cc2 = CFF_ChipCap2.CFF_ChipCap2(mb, ChipCap2_i2cAddr, ChipCap2_PowerPin, ChipCap2_ReadyPin, ChipCap2_AlarmLowPin, ChipCap2_AlarmHighPin) # init ChipCap2 class and set all init variables
+# Alternatively you could just do: cc2 = CFF_ChipCap2.CFF_ChipCap2(mb)  if you are using all the default settings
+
+cc2.startCommandMode() # put ChipCap2 in Command Mode
+if cc2.status == CFF_ChipCap2.CCF_CHIPCAP2_STATUS_COMMANDMODE:  # check to make sure it went into command mode
+    print "ChipCap2 is now in command mode"
+    print "Get Current Alarm Values"
+    alow = cc2.getAlarmLowVal()
+    print "Alarm Low: ", alow
+    ahigh = cc2.getAlarmHighVal()
+    print "Alarm High: ", ahigh
+    mb.pausems(100)
+    print "Setting New Alarm Values"
+    cc2.setAlarmHighVal(29) # Set High Alarm to 29 C
+    mb.pausems(100)
+    cc2.setAlarmLowVal(18) # Set Low Alarm to 18 C
+    mb.pausems(100)
+
+    print "Going back to Normal Mode"
+    cc2.startNormalMode() # Go back into Normal Mode
+    mb.pausems(100)
+else:
+    print "ChipCap2 is in Normal Mode"
+
+cc2.power(1)                        # turn ChipCap2 power on
+mb.pausems(500)
+
+while 1:
+    if cc2.dataReady() == True: # if data is ready for reading
+        cc2.readSensor()     # read sensor data
+        print "Humidity: ", cc2.humidity
+        print "Temperature: ", cc2.temperatureC, "C"
+        print "Temperature: ", cc2.temperatureF, "F"
+    mb.pausems(5000)
+
---
+++ Python/Library/CFF_ChipCap2.py
@@ -0,0 +1,178 @@
+"""
+    This library is for the GE ChipCap2 Humidity & Temperature Sensor
+
+    0 ~ 100% Relative Humidity  (7 Sec. response time)
+    -40 C ~ 125 C  Temperature (5 Sec. response time)
+    Low and High Alarm settings.
+
+    Various models with analog and digital output @ 3.3V or 5V operation.
+
+    Digi-Key Part Number: 235-1339-ND   (Digital i2c, 5V)
+    Breakout Boards available at: www.circuitsforfun.com
+
+    Written by: Richard Wardlow for Circuits for Fun, LLC
+    GNU GPL, include above text in redistribution
+"""
+
+VERSION = '1.0.0'
+
+import time
+
+CFF_CHIPCAP2_DEFAULT_ADDR = 0x28
+CCF_CHIPCAP2_STATUS_VALID = 0
+CCF_CHIPCAP2_STATUS_STALE = 1
+CCF_CHIPCAP2_STATUS_COMMANDMODE = 2
+CCF_CHIPCAP2_COMMAND_BUSY = 0
+CCF_CHIPCAP2_COMMAND_SUCCESS = 1
+CCF_CHIPCAP2_COMMAND_ERROR = 2
+CCF_CHIPCAP2_EEPROM_ERROR_CORRECTED = 2
+CCF_CHIPCAP2_EEPROM_ERROR_UNCORRECTED = 4
+CCF_CHIPCAP2_EEPROM_ERROR_RAM = 8
+CCF_CHIPCAP2_EEPROM_ERROR_CONFIG = 16
+
+
+class CFF_ChipCap2:
+    def __init__(self, mb=None, i2cAddr=CFF_CHIPCAP2_DEFAULT_ADDR, powerPin=1, readyPin=2, alarmLowPin=None, alarmHighPin=None):
+        self.i2caddr = i2cAddr
+        self.pwrpin = powerPin
+        self.readypin = readyPin
+        self.alowpin = alarmLowPin
+        self.ahighpin = alarmHighPin
+        self.mb = mb
+        self.temperatureC = 0
+        self.temperatureF = 0
+        self.humidity = 0
+        self.status = 0
+        
+        if self.mb != None:
+            self.mb.digitalState(4,1)
+            if self.alowpin != None:
+                self.mb.digitalState(self.alowpin, 1)
+            if self.ahighpin != None:
+                self.mb.digitalState(self.ahighpin, 1)
+            self.mb.digitalState(self.readypin, 1)
+
+    def startCommandMode(self):
+        if self.mb != None:
+            self.mb.pinLow(self.pwrpin)
+            self.mb.pausems(50)
+            self.mb.pinHigh(self.pwrpin)
+            self.mb.pausems(1)
+            self.mb.i2cWrite((self.i2caddr << 1), [0xA0, 0, 0])
+            self.mb.pausems(5)
+            self.readSensor()
+
+    def startNormalMode(self):
+        if self.mb != None:
+            self.mb.i2cWrite((self.i2caddr << 1),[0x80, 0, 0])
+            self.mb.pausems(5)
+            self.readSensor()
+
+    def checkAlarmLow(self):
+        if self.mb != None:
+            if self.mb.digitalRead(self.alowpin) == 1:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def setAlarmLowVal(self, alarmValue):
+        if self.mb != None:
+            self.readSensor()
+            if self.status == CCF_CHIPCAP2_STATUS_COMMANDMODE:
+                alowon = (alarmValue + 40) * 99
+                lowByte = alowon & 255
+                hiByte = alowon >> 8
+                self.mb.i2cWrite((self.i2caddr << 1),[0x5A, hiByte, lowByte])
+                self.mb.pausems(2)
+                alowoff = ((alarmValue + 1) + 40) * 99
+                lowByte = alowoff & 255
+                hiByte = alowoff >> 8
+                self.mb.i2cWrite((self.i2caddr << 1),[0x5B, hiByte, lowByte])
+                self.mb.pausems(5)
+                
+
+    def getAlarmLowVal(self):
+        if self.mb != None:
+            self.readSensor()
+            if self.status == CCF_CHIPCAP2_STATUS_COMMANDMODE:
+                self.mb.i2cWrite((self.i2caddr << 1),[0x1A, 0, 0])
+                self.mb.pausems(1)
+                edat = self.mb.i2cRead((self.i2caddr << 1) + 1, 3)
+                eALow = (((edat[1] << 8) + (edat[2])) / 99) - 40
+                return eALow
+
+    def checkAlarmHigh(self):
+        if self.mb != None:
+            if self.mb.digitalRead(self.ahighpin) == 1:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def setAlarmHighVal(self, alarmValue):
+        if self.mb != None:
+            self.readSensor()
+            if self.status == CCF_CHIPCAP2_STATUS_COMMANDMODE:
+                alowon = (alarmValue + 40) * 99
+                lowByte = alowon & 255
+                hiByte = alowon >> 8
+                self.mb.i2cWrite((self.i2caddr << 1),[0x58, hiByte, lowByte])
+                self.mb.pausems(2)
+                alowoff = ((alarmValue + 1) + 40) * 99
+                lowByte = alowoff & 255
+                hiByte = alowoff >> 8
+                self.mb.i2cWrite((self.i2caddr << 1),[0x59, hiByte, lowByte])
+                self.mb.pausems(5)
+
+    def getAlarmHighVal(self):
+        if self.mb != None:
+            self.readSensor()
+            if self.status == CCF_CHIPCAP2_STATUS_COMMANDMODE:
+                self.mb.i2cWrite((self.i2caddr << 1),[0x18, 0, 0])
+                self.mb.pausems(1)
+                edat = self.mb.i2cRead((self.i2caddr << 1) + 1, 3)
+                eAHigh = (((edat[1] << 8) + (edat[2])) / 99) - 40
+                return eAHigh
+
+    def power(self, onOff):
+        """If using a digital pin for power use this function to turn power on and off
+        Usage: power(onOff)
+        Example Off: power(0)
+        Example On: power(1)"""
+        if self.mb != None:
+            if onOff == 0:
+                self.mb.pinLow(self.pwrpin)
+            elif onOff == 1:
+                self.mb.pinHigh(self.pwrpin)
+
+    def readSensor(self):
+        """Read Sensor Data
+        Usage: readSensor()"""
+        if self.mb != None:
+            rht = self.mb.i2cRead((self.i2caddr << 1) + 1, 4)
+            self.status = rht[0] >> 6
+            rh = ((rht[0] & 63) * 256) + rht[1]
+            rh = (rh / 16384.0) * 100.0
+            t = (rht[2] * 64) + rht[3] / 4
+            t = (t / 16384.0) * 165.0 - 40
+            tf = t * 1.8 + 32
+            self.humidity = rh
+            self.temperatureC = t
+            self.temperatureF = tf
+
+    def dataReady(self):
+        """Check if data is ready for reading
+        Usage: dataReady()"""
+        if self.mb != None:
+            if self.mb.digitalRead(self.readypin) == 1:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+
+
